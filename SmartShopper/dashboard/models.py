41afa3ef69c3e937c28137bcd430b27410d5bc40
gamebot770 <31254203+gamebot770@users.noreply.github.com>
2019-08-09 23:21:29 -0500

    Init 3

--- SmartShopper/dashboard/models.py
+++ SmartShopper/dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=900)
+    address = models.CharField(max_length=900)
+    numOfCustomers = models.IntegerField()
