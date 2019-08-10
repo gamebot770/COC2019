7f747a292cafe6e6cccb52e43c035c4845a7585d
Kevovaz <agraton12@gmail.com>
2019-08-10 04:39:27 -0500

    fix conflict

--- SmartShopper/dashboard/views.py
+++ SmartShopper/dashboard/views.py
@@ -6,10 +6,6 @@ from dashboard.models import *
 
 import json
 # Create your views here.
-def dashboard(request):
-    pk = 0
-    user.object.get(username= 'gamebot')
-    return HttpResponse("Hi")
 
 def itemView(request):
     pk = 0
